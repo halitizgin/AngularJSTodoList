ca6f743c566e32f2576b77b221394781a05898a1
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-08 20:27:55 +0300

    AngularJS Ders 9 Kısım 3 - checkbox değişiklik işlemleri

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
 var app = angular.module("TodoList", []);
 app.controller('TodoListController', $scope => {
     $scope.yapilacaklar = [
-        { name: 'Toplantı', okay: false },
-        { name: 'İş', okay: true }
+        { id: 1, name: 'Toplantı', okay: false },
+        { id: 2, name: 'İş', okay: true }
     ];
+
+    $scope.checkClick = id => {
+        const index = $scope.yapilacaklar.findIndex(yapilacak => yapilacak.id === id);
+        if (index !== -1){
+            const okay = $scope.yapilacaklar[index].okay;
+            $scope.yapilacaklar[index].okay = !okay;
+            console.log($scope.yapilacaklar);
+        }
+    }
 });
